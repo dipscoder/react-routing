4f0cf6cabebc1ca0b510c3df79ae5ddfdb9b1a04
dipscoder <dipeshjaiswal140@gmail.com>
2020-12-29 15:56:06 +0530

    Problem we face if we do not use Switch

--- src/App.js
+++ src/App.js
@@ -11,11 +11,21 @@ function App() {
     <Router >
       <div className="App">
         <Nav />
+        <Route path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/shop" component={Shop} />
       </div>
     </Router>
   );
 }
+
+// ! Problem - If we don't use the switch then the Route will render the Home page even if we go to the url "/about" or "/shop"
+// * Because as soon as Route encounter the "/" it will render the Home page , it will not check the text present after the "/"  
+// Explanation - https://www.youtube.com/watch?v=Law7wfdg_ls&t=155s 10:50
+const Home = () => (
+  <div>
+    <h1>Home page</h1>
+  </div>
+)
 
 export default App;
