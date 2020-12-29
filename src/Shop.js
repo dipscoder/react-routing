15b49bcf71ffa24e623ac9900455c6dd4a31af34
dipscoder <dipeshjaiswal140@gmail.com>
2020-12-29 17:03:38 +0530

    Fecth data from the fortnite api

--- src/Shop.js
+++ src/Shop.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 
 function Shop() {
+    
+    useEffect(() => {
+        fetchItems()
+    }, [])
+    
+    const [items, setItems] = useState([])
+
+    const fetchItems = async () => {
+        const fetchedData = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get')
+        const fetchedItems = await fetchedData.json()
+        console.log(fetchedItems.data);
+        setItems(fetchedItems.data)
+
+    }
+
     return (
         <div>
-            <h1>Shop</h1>
+            {items.slice(0,10).map((item) => (
+                <h1>{item.item.name}</h1>
+            ))}
         </div>
     )
 }
