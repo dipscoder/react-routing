31d68e02c610a657a467d84bb46e579a23714472
dipscoder <dipeshjaiswal140@gmail.com>
2020-12-29 15:34:12 +0530

    Add style to the navbar

--- src/Nav.js
+++ src/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
         <div>
             <nav>
                 <h4>Logo</h4>
-                <ul>
+                <ul className="nav-links">
                     <li>About</li>
                     <li>Shop</li>
                 </ul>
