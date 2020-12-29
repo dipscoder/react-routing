fcc5eeac3f93e46b0bcad68fbed0e4e1bb63797a
dipscoder <dipeshjaiswal140@gmail.com>
2020-12-29 15:29:42 +0530

    Add more components

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div>
+            <nav>
+                <h4>Logo</h4>
+                <ul>
+                    <li>About</li>
+                    <li>Shop</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
