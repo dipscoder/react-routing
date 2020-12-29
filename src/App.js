beddf382db9d1d64ba8aacdcb141e2e074762641
dipscoder <dipeshjaiswal140@gmail.com>
2020-12-29 15:45:10 +0530

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import About from "./About";
 import Nav from "./Nav"
 import Shop from "./Shop";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <About />
-      <Shop />
-    </div>
+    <Router >
+      <div className="App">
+        <Nav />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+      </div>
+    </Router>
   );
 }
 
